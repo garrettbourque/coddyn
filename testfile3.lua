70cf0f9f44a917f0649a7fe7fd5b77db1cef435c
Garrett Bourque <bourque.garrett@gmail.com>
2026-06-24 17:34:13 -0500

    episode 4 commit

--- testfile3.lua
+++ testfile3.lua
@@ -1,2 +1,30 @@
-local num1 =10
-print (num1)
+--[[local num1 =10
+local num2 =5
+print (num1+num2)
+print (num1-num2)
+print (num1/num2)
+print (num1*num2)
+print (num1%num2)
+print (num1^num2)
+]]
+--[[
+local player1Score = 150
+local player2Score = 200
+
+print(player1Score == player2Score)
+print(player1Score < player2Score)
+print(player1Score > player2Score)]]
+
+local playerLevel=5
+local playerHealth=50
+
+if playerLevel>3 and playerHealth>30 then
+    print("You are eligible for a bonus!")
+    print("Bonus unlocked!")
+else
+    print("You need more health or level")
+end
+
+print(playerLevel)
+playerLevel=8
+print(playerLevel)

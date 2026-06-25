ba5bf0455d23765e9a754d47e1ef4be9c1083c0e
Garrett Bourque <bourque.garrett@gmail.com>
2026-06-24 19:03:26 -0500

    episode 8 commit

---
+++ episode7.lua
@@ -0,0 +1,27 @@
+--w is for write mode
+--a is for append mode
+--r is for read mode
+--[[
+local file = io.open("myfile.txt","w")
+file:write("now")
+file:write("learning file handling")
+file:close()
+
+local file = io.open("myfile.txt","a")
+file:write("now")
+file:write("learning file handling")
+file:close()
+
+]]
+local file = io.open("myfile.txt","r")
+
+if file then
+    local content = file:read("*line") -- read the entire file
+        local content1 = file:read("*line") -- read the entire file
+print(content)
+print(content1)
+file:close()
+else
+    print("File not found!")
+
+end
